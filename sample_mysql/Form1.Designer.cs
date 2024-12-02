8b107ea0fd4f11284543c6176876ce2b92cc8a78
ksn01234jp <kusano.you@gmail.com>
2024-12-02 10:54:22 +0900

    test

--- sample_mysql/Form1.Designer.cs
+++ sample_mysql/Form1.Designer.cs
@@ -28,13 +28,47 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(12, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(101, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Service Name";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+			this.textBox1.Location = new System.Drawing.Point(145, 17);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(160, 23);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "MySQL91";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(661, 358);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
 	}
 }
 
